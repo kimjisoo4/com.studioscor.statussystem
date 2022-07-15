8892ff6c635a1f5ae58c1de8ce2d98da9c62b2ff
kimjisoo4 <playcastle.index@gmail.com>
2022-07-15 12:32:02 +0900

    Upload

---
+++ Scripts/StatusCostSO.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace KimScor.StatusSystem
+{
+    [System.Serializable]
+    public class StatusCostSO
+    {
+        [SerializeField] private StatusTag _StatusTag;
+        [SerializeField] private EConsumeType _ConsumeType;
+        [SerializeField] private float _ConsumePoint;
+        [SerializeField, Range(0f, 100f)] private float _ConsumeRatioPoint;
+
+        public StatusTag StatusTag => _StatusTag;
+        public EConsumeType ConsumeType => _ConsumeType; 
+        public float ConsumePoint => _ConsumePoint;
+        public float ConsumeRatioPoint => _ConsumeRatioPoint;
+
+        public StatusCostSpec CreateSpec(StatusSystem statusSystem)
+        {
+            var spec = new StatusCostSpec(this, statusSystem);
+
+            return spec;
+        }
+    }
+}
+
---
+++ Scripts/StatusSystem.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace KimScor.StatusSystem
+{
+    public class StatusSystem : MonoBehaviour
+    {
+        #region Events
+        public delegate void StatusEventHandler(StatusSystem statusSystem, Status status);
+        #endregion
+
+        public Dictionary<StatusTag, Status> Statuses = new Dictionary<StatusTag, Status>();
+
+        public event StatusEventHandler OnAddedStatus;
+
+        public Status CreateOrSetValue(StatusTag Tag, float minValue, float maxValue)
+        {
+            if (!Statuses.TryGetValue(Tag, out Status value))
+            {
+                value = new Status(Tag, minValue, maxValue);
+
+                Statuses.Add(Tag, value);
+
+                OnAddStatus(value);
+            }
+            else
+            {
+                value.SetPoint(minValue, maxValue);
+            }
+
+            return value;
+        }
+
+        public Status GetValue(StatusTag Tag)
+        {
+            if (!Tag)
+            {
+                return null;
+            }
+
+            if (Statuses.TryGetValue(Tag, out Status value))
+                return value;
+
+            return null;
+        }
+
+        public Status GetOrCreateValue(StatusTag Tag, float minValue = 0f, float maxValue = 0f)
+        {
+            if (!Tag)
+            {
+                return null;
+            }
+
+            if (Statuses.TryGetValue(Tag, out Status value))
+                return value;
+            else
+            {
+                value = new Status(Tag, minValue, maxValue);
+
+                Statuses.Add(Tag, value);
+
+                OnAddStatus(value);
+
+                return value;
+            }
+        }
+        public Status GetOrCreateValue(FInitializationStatus Status)
+        {
+            return GetOrCreateValue(Status.StatusTag, Status.MinValue, Status.MaxValue);
+        }
+
+        #region Callback
+        public void OnAddStatus(Status status)
+        {
+            OnAddedStatus?.Invoke(this, status);
+        }
+        #endregion
+    }
+}
