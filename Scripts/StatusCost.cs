dc766a05a47ab7b4a7b92cfb4e814b4a06cd4f7b
kimjisoo4 <58765606+kimjisoo4@users.noreply.github.com>
2022-08-13 20:10:36 +0900

    다행히 비가 오지 않는다
    [ Changed ]
     StatusCostBase
     - abstract 클래스로 일부 값을 override 해서 스테이터스 코스트를 처리하는 식으로 교체함. 교체 이유는 값 변경을 런타임에 동적으로 조절하고 싶은데, 그 처리를 위해서 상속 받는 식으로 변경함.

--- Scripts/StatusCost.cs
+++ Scripts/StatusCost.cs
@@ -2,22 +2,21 @@
 
 namespace KimScor.StatusSystem
 {
-    [System.Serializable]
-    public class StatusCostSO
+    [CreateAssetMenu(menuName ="Status/New Status Cost", fileName = "Cost_")]
+    public class StatusCost : ScriptableObject
     {
         [SerializeField] private StatusTag _StatusTag;
         [SerializeField] private EConsumeType _ConsumeType;
         [SerializeField] private float _ConsumePoint;
         [SerializeField, Range(0f, 100f)] private float _ConsumeRatioPoint;
 
-        public StatusTag StatusTag => _StatusTag;
         public EConsumeType ConsumeType => _ConsumeType; 
         public float ConsumePoint => _ConsumePoint;
         public float ConsumeRatioPoint => _ConsumeRatioPoint;
 
         public StatusCostSpec CreateSpec(StatusSystem statusSystem)
         {
-            var spec = new StatusCostSpec(this, statusSystem);
+            var spec = new StatusCostSpec(this, statusSystem, _StatusTag);
 
             return spec;
         }
--- Scripts/StatusCostSpec.cs
+++ Scripts/StatusCostSpec.cs
@@ -1,146 +1,20 @@
-﻿using UnityEngine;
-
-namespace KimScor.StatusSystem
+﻿namespace KimScor.StatusSystem
 {
-    public enum EConsumeType
-    {
-        [Tooltip("절대값으로 스테이터스를 차감함")] Absolute,
-        [Tooltip("최대 값을 비율로 차감함")] RatioInMax,
-        [Tooltip("현재 값을 비율로 차감함")] RatioInCurret,
-    }
-
     [System.Serializable]
-    public class StatusCost
+    public class StatusCostSpec : StatusCostBase
     {
-        [SerializeField] private EConsumeType _ConsumeType;
-        [SerializeField] private float _ConsumePoint;
-        [SerializeField] private float _ConsumeRatioPoint;
-        [SerializeField] private Status _Status;
-        public StatusCost(StatusSystem statusSystem, StatusTag statusTag, EConsumeType consumeType, float consumePoint, float consumeRatioPoint)
-        {
-            _Status = statusSystem.GetOrCreateValue(statusTag);
-
-            _ConsumeType = consumeType;
-            _ConsumePoint = consumePoint;
-            _ConsumeRatioPoint = consumeRatioPoint;
-        }
-        public EConsumeType ConsumeType => _ConsumeType;
-        public float ConsumePoint => _ConsumePoint;
-        public float ConsumeRatioPoint => _ConsumeRatioPoint;
-
-        public void ConsumeCost()
-        {
-            if (_Status == null)
-                return;
-
-            _Status.DamagePoint(CurrentConsumeCost());
-        }
-
-        public bool CanConsumeCost()
-        {
-            if (_Status == null)
-                return false;
-
-            float consumePoint = CurrentConsumeCost();
-
-            if (consumePoint < 0)
-                return false;
-
-            return _Status.CurrentPoint >= consumePoint;
-        }
-
-        public float CurrentConsumeCost()
-        {
-            if (_Status == null)
-                return -1f;
-
-            switch (ConsumeType)
-            {
-                case EConsumeType.Absolute:
-                    return ConsumePoint;
-                case EConsumeType.RatioInMax:
-                    return _Status.MaxPoint * ConsumeRatioPoint * 0.01f;
-                case EConsumeType.RatioInCurret:
-                    return _Status.CurrentPoint * ConsumeRatioPoint * 0.01f;
-                default:
-                    return -1f;
-            }
-        }
-    }
-
-    [System.Serializable]
-    public class StatusCostSpec
-    {
-        private StatusCostSO _StatusCost;
-        public StatusTag StatusTag => _StatusCost.StatusTag;
-        public EConsumeType ConsumeType => _StatusCost.ConsumeType;
-        public float ConsumePoint => _StatusCost.ConsumePoint;
-        public float ConsumeRatioPoint => _StatusCost.ConsumeRatioPoint;
+        private StatusCost _StatusCost;
+        public override EConsumeType ConsumeType => _StatusCost.ConsumeType;
+        public override float ConsumePoint => _StatusCost.ConsumePoint;
+        public override float ConsumeRatioPoint => _StatusCost.ConsumeRatioPoint;
 
         private StatusSystem _StatusSystem;
         public StatusSystem StatusSystem => _StatusSystem;
 
-        private Status _Status;
-
-        public Status Status
-        {
-            get
-            {
-                if (_Status == null)
-                {
-                    if (!StatusSystem)
-                        return null;
-
-                    _Status = StatusSystem.GetOrCreateValue(_StatusCost.StatusTag);
-                }
-
-                return _Status;
-            }
-        }
-        
-        public StatusCostSpec(StatusCostSO statusCostSO, StatusSystem statusSystem)
+        public StatusCostSpec(StatusCost statusCostSO, StatusSystem statusSystem, StatusTag statusTag) : base(statusSystem, statusTag)
         {
             _StatusCost = statusCostSO;
             _StatusSystem = statusSystem;
-        }
-
-        public void ConsumeCost()
-        {
-            if (!StatusSystem || Status == null)
-                return;
-
-            Status.DamagePoint(CurrentConsumeCost());
-        }
-
-        public bool CanConsumeCost()
-        {
-            if (!StatusSystem || Status == null)
-                return false;
-
-            float consumePoint = CurrentConsumeCost();
-
-            if (consumePoint < 0)
-                return false;
-
-            return Status.CurrentPoint >= consumePoint;
-        }
-
-        public float CurrentConsumeCost()
-        {
-            if (!StatusSystem || Status == null)
-                return -1f;
-
-            switch (ConsumeType)
-            {
-                case EConsumeType.Absolute:
-                    return ConsumePoint;
-                case EConsumeType.RatioInMax:
-                    return Status.MaxPoint * ConsumeRatioPoint * 0.01f;
-                case EConsumeType.RatioInCurret:
-                    return Status.CurrentPoint * ConsumeRatioPoint * 0.01f;
-                default:
-                    return -1f;
-            }
         }
     }
 }
